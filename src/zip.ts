653f7e51b6f77e658654079dfb16db13411143f7
Michinosuke <michinosuke.info@gmail.com>
2024-02-18 20:24:27 +0900

    fix promises

--- src/zip.ts
+++ src/zip.ts
@@ -3,24 +3,25 @@ import path from "path";
 import archiver from "archiver";
 
 export const zip = async (dateFileName: string) => {
-  const archive = archiver("zip", {
-    zlib: { level: 9 },
+  return new Promise((resolve, reject) => {
+    const archive = archiver("zip", {
+      zlib: { level: 9 },
+    });
+
+    const sqlFilePath = path.join(__dirname, `../database/${dateFileName}.sql`);
+    const zipFilePath = path.join(__dirname, `../database/${dateFileName}.zip`);
+
+    const outputZipStream = fs.createWriteStream(zipFilePath);
+    outputZipStream.on("close", () => {
+      console.log(`[done] ${sqlFilePath} → ${zipFilePath}`);
+      resolve({ zipFilePath });
+    });
+    archive.on("error", (err) => {
+      reject(err);
+    });
+
+    archive.file(sqlFilePath, { name: `${dateFileName}.sql` });
+    archive.pipe(outputZipStream);
+    archive.finalize();
   });
-
-  const sqlFilePath = path.join(__dirname, `../database/${dateFileName}.sql`);
-  const zipFilePath = path.join(__dirname, `../database/${dateFileName}.zip`);
-
-  const outputZipStream = fs.createWriteStream(zipFilePath);
-  outputZipStream.on("close", () => {
-    console.log(`[done] ${sqlFilePath} → ${zipFilePath}`);
-  });
-  archive.on("error", (err) => {
-    throw err;
-  });
-
-  archive.file(sqlFilePath, { name: `${dateFileName}.sql` });
-  archive.pipe(outputZipStream);
-  await archive.finalize();
-
-  console.log("finalize");
 };
